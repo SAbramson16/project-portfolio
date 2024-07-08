0947f423ae2894b5f629132542316a068f4b1032
Sharon Abramson <sharabramson@gmail.com>
2024-07-08 19:52:56 -0400

    ran build

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
                     <div className="text-center p-10">
                         <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">Sharon Abramson</h2>
                         <h3 className="text-2xl py-2 md:text-3xl dark:text-white">Developer | Problem Solver | Lifelong Learner</h3>
-                        <p className="text-md py-5 leading-6 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-white">Full stack developer passionate about contributing to innovative projects. Let's get in touch!</p>
+                        <p className="text-md py-5 leading-6 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-white">Full stack developer passionate about contributing to innovative projects. Let&apos;s get in touch!</p>
                     </div>
 
                     <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
@@ -47,7 +47,7 @@ export default function Home() {
                     </div>
 
                     <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-60 h-80 mt-20 overflow-hidden md:h-96 md:w-76">
-                        <Image src={photo} layout="fill" objectFit="cover" />
+                        <Image src={photo} layout="fill" objectFit="cover" alt="bio-pic"/>
                     </div>
                 </section>
 
@@ -63,7 +63,7 @@ export default function Home() {
                         <div className="my-3 bg-gray-200 relative shadow-lg p-10 rounded-xl basis-1/3 flex-1 dark:bg-gray-800">
                             <h3 className="inset-0 flex items-center justify-center text-teal-800 text-2xl font-bold mb-2 dark:text-teal-400">Weather Dashboard</h3>
                             <a href="https://sabramson16.github.io/weather-dashboard">
-                                <Image className="rounded-lg object-cover" src={weather} width={'100%'} height={'100%'} layout={'responsive'}></Image>
+                                <Image className="rounded-lg object-cover" src={weather} width={'100%'} height={'100%'} layout={'responsive'} alt="weather"></Image>
                             </a>
                             <h4 className="text-md pt-1 inset-0 flex items-center justify-center font-bold hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"><a href="https://github.com/SAbramson16/weather-dashboard">GitHub Repository</a></h4>
                         </div>
@@ -71,7 +71,7 @@ export default function Home() {
                         <div className="my-3 bg-gray-200 relative shadow-lg p-10 rounded-xl basis-1/3 flex-1 dark:bg-gray-800">
                             <h3 className="inset-0 flex items-center justify-center text-teal-800 text-2xl font-bold mb-2 dark:text-teal-400">Code Quiz</h3>
                             <a href="https://sabramson16.github.io/code-quiz/">
-                                <Image className="w-full h-auto" src={quiz} width={'100%'} height={'100%'} layout={'responsive'} ></Image>
+                                <Image className="w-full h-auto" src={quiz} width={'100%'} height={'100%'} layout={'responsive'} alt="code-quiz"></Image>
                             </a>
                             <h4 className="text-md pt-1 inset-0 flex items-center justify-center font-bold hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"><a href="https://github.com/SAbramson16/code-quiz">GitHub Repository</a></h4>
                         </div>
@@ -79,7 +79,7 @@ export default function Home() {
                         <div className="my-3 bg-gray-200 relative shadow-lg p-10 rounded-xl basis-1/3 flex-1 dark:bg-gray-800">
                             <h3 className="inset-0 flex items-center justify-center text-teal-800 text-2xl font-bold mb-2 dark:text-teal-400">Employee Tracker</h3>
                             <a href="https://drive.google.com/file/d/14cqS4adv-N9hgT42FDJjQmoMoK1AFiwq/view">
-                                <Image className="w-full h-auto" src={employee} width={'100%'} height={'100%'} layout={'responsive'} ></Image>
+                                <Image className="w-full h-auto" src={employee} width={'100%'} height={'100%'} layout={'responsive'} alt="employee-tracker"></Image>
                             </a>
                             <h4 className="text-md pt-1 inset-0 flex items-center justify-center font-bold hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"><a href="https://github.com/SAbramson16/employee-tracker">GitHub Repository</a></h4>
                         </div>
@@ -87,7 +87,7 @@ export default function Home() {
                         <div className="my-3 bg-gray-200 relative shadow-lg p-10 rounded-xl basis-1/3 flex-1 dark:bg-gray-800">
                             <h3 className="inset-0 flex items-center justify-center text-teal-800 text-2xl font-bold mb-2 dark:text-teal-400">Random Password Generator</h3>
                             <a href="https://sabramson16.github.io/password-generator/">
-                                <Image className="w-full h-auto" src={password} width={'100%'} height={'100%'} layout={'responsive'}></Image>
+                                <Image className="w-full h-auto" src={password} width={'100%'} height={'100%'} layout={'responsive'} alt="password-gen"></Image>
                             </a>
                             <h4 className="text-md pt-1 inset-0 flex items-center justify-center font-bold hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"><a href="https://github.com/SAbramson16/password-generator">GitHub Repository</a></h4>
                         </div>
@@ -95,7 +95,7 @@ export default function Home() {
                         <div className="my-3 bg-gray-200 relative shadow-lg p-10 rounded-xl basis-1/3 flex-1 dark:bg-gray-800">
                             <h3 className="inset-0 flex items-center justify-center text-teal-800 text-2xl font-bold mb-2 dark:text-teal-400">Doodle Drop</h3>
                             <a href="https://doodledrop-7357eeb23db1.herokuapp.com/">
-                                <Image className="w-full h-auto" src={doodle} width={'100%'} height={'100%'} layout={'responsive'}></Image>
+                                <Image className="w-full h-auto" src={doodle} width={'100%'} height={'100%'} layout={'responsive'} alt="doodle-drop"></Image>
                             </a>
                             <h4 className="text-md pt-1 inset-0 flex items-center justify-center font-bold hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"><a href="https://github.com/SAbramson16/doodle-drop">GitHub Repository</a></h4>
                         </div>
@@ -103,7 +103,7 @@ export default function Home() {
                         <div className="my-3 bg-gray-200 relative shadow-lg p-10 rounded-xl basis-1/3 flex-1 dark:bg-gray-800">
                             <h3 className="inset-0 flex items-center justify-center text-teal-800 text-2xl font-bold mb-2 dark:text-teal-400">BookNook</h3>
                             <a href="https://book-club-u6w0.onrender.com">
-                                <Image className="w-full h-auto" src={booknook} width={'100%'} height={'100%'} layout={'responsive'}></Image>
+                                <Image className="w-full h-auto" src={booknook} width={'100%'} height={'100%'} layout={'responsive'} alt="booknook"></Image>
                             </a>
                             <h4 className="text-md pt-1 inset-0 flex items-center justify-center font-bold hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"><a href="https://github.com/SAbramson16/book-nook">GitHub Repository</a></h4>
                         </div>
